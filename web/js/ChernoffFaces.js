e129061f9908226f6d698d4902e6a3936d1a62d8
Błażej <b.chodorek@ecrosio.com>
2017-12-09 10:52:32 +0100

    dynamicznie generowany formularz

--- web/js/ChernoffFaces.js
+++ web/js/ChernoffFaces.js
@@ -4,15 +4,21 @@
 function ChernoffFaces() {
 
     /**
-     * Canvas id
-     * @type {string}
+     * Uruchomienie aplikacji.
      */
-    var canvasId = "mapCanvas";
+    this.run = function () {
+        drawMap();
+        generateFileInput();
+    };
 
     /**
      * Funkcja rysująca mapę z pliku "map.png"
      */
-    this.drawMap = function () {
+    function drawMap() {
+
+        $(".chernoff-map-container").html(
+            "<canvas id=\"mapCanvas\" width=\"660\" height=\"615\"></canvas>"
+        );
 
         var map = new Image();
         map.src = "map.png";
@@ -22,7 +28,7 @@ function ChernoffFaces() {
         dynamicCanvas.height = "620";
         dynamicCanvas.width = "660";
 
-        var canvas = document.getElementById(canvasId);
+        var canvas = document.getElementById("mapCanvas");
         var ctx = canvas.getContext("2d");
 
         map.onload = function () {
@@ -31,20 +37,188 @@ function ChernoffFaces() {
             ctx = ctx.drawImage(dynamicCanvas, 0, 0);
             // window.open(canvas.toDataURL());
         };
-    };
+    }
+
+    /**
+     * Funkcja generująca input do wczytywania pliku .json oraz go wczytująca.
+     */
+    function generateFileInput() {
+
+        /**
+         * Input do wczytania pliku, akceptowane są tylko pliki z rozszerzeniem .json.
+         */
+        $(".chernoff-input-file-container").html(
+            "<ul class=\"list-group\">\n" +
+            "<li class=\"list-group-item\">\n" +
+            "<label class=\"fileContainer\">\n" +
+            "<button type=\"button\" class=\"btn btn-primary btn-block\">Wczytaj nowy plik</button>\n" +
+            "<input type=\"file\" id=\"file-input\" accept=\".json\"/>\n" +
+            "</label>\n" +
+            "<div class=\"alert alert-success alert-dismissable fade in hidden\" id=\"alertFile-success\">\n" +
+            "<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>\n" +
+            "<strong>Plik wczytano poprawnie.</strong>\n" +
+            "</div>\n" +
+            "<div class=\"alert alert-danger alert-dismissable fade in hidden\" id=\"alertFile-danger\">\n" +
+            "<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>\n" +
+            "<strong>Zapomniałeś o wczytaniu pliku.</strong>\n" +
+            "</div>\n" +
+            "</li>" +
+            "</ul>"
+        );
+
+        /**
+         * Funkcja do wczytania pliku .json.
+         * Po wczytaniu pliku zostaje generowany formularz.
+         * @param e
+         */
+        function readSingleFile(e) {
+            var file = e.target.files[0];
+            if (!file) {
+                return;
+            }
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#alertFile-success').removeClass('hidden');
+
+                if ($('#alertFile-success').hasClass('file-loaded')) {
+                    $('#alertFile-success').addClass('hidden');
+                }
+                $('#alertFile-danger').addClass('hidden');
+                $('#alertFile-success').addClass('file-loaded');
+
+                contents = e.target.result;
+                generateForm(JSON.parse(contents).wojewodztwa);
+                $("#generateFaces").on('click', function (event) {
+                    drawFaces(JSON.parse(contents).wojewodztwa);
+                });
+            };
+            reader.readAsText(file);
+        }
+
+        document.getElementById('file-input').addEventListener('change', readSingleFile, false);
+
+        $("#generateFaces").on('click', function (event) {
+
+            if ($('#alertFile-success').hasClass('hidden')) {
+                $('#alertFile-danger').removeClass('hidden');
+            }
+        });
+    }
+
+    /**
+     * Funkcja dynamicznie generująca formularz na podstawie pliku .json.
+     */
+    function generateForm(data) {
+
+        /**
+         * Formularz.
+         */
+        $(".chernoff-form-container").html(
+            "<ul class=\"list-group\">\n" +
+            "                    <li class=\"list-group-item\">\n" +
+            "                        <span class=\"error\" id=\"error-eyes\"></span>\n" +
+            "                        <div class=\"row\">\n" +
+            "                            <div class=\"col-sm-1\" style=\"padding: 5px; padding-right: 40px\">\n" +
+            "                                <h4>oczy:</h4>\n" +
+            "                            </div>\n" +
+            "                            <div class=\"col-sm-9 panel-list-item\">\n" +
+            "                                <form class=\"chernoff-form\">\n" +
+            "                                    <select class=\"form-control\" id=\"eyes\"></select>\n" +
+            "                                </form>\n" +
+            "                            </div>\n" +
+            "                        </div>\n" +
+            "                    </li>\n" +
+            "\n" +
+            "                    <li class=\"list-group-item\">\n" +
+            "                        <span class=\"error\" id=\"error-mounth\"></span>\n" +
+            "                        <div class=\"row\">\n" +
+            "                            <div class=\"col-sm-1\" style=\"padding: 5px; padding-right: 40px\">\n" +
+            "                                <h4>usta:</h4>\n" +
+            "                            </div>\n" +
+            "                            <div class=\"col-sm-9 panel-list-item\">\n" +
+            "                                <form class=\"chernoff-form\">\n" +
+            "                                    <select class=\"form-control\" id=\"mounth\"></select>\n" +
+            "                                </form>\n" +
+            "                            </div>\n" +
+            "                        </div>\n" +
+            "                    </li>\n" +
+            "\n" +
+            "                    <li class=\"list-group-item\">\n" +
+            "                        <span class=\"error\" id=\"error-nose\"></span>\n" +
+            "                        <div class=\"row\">\n" +
+            "                            <div class=\"col-sm-1\" style=\"padding: 5px; padding-right: 40px\">\n" +
+            "                                <h4>nos:</h4>\n" +
+            "                            </div>\n" +
+            "                            <div class=\"col-sm-9 panel-list-item\">\n" +
+            "                                <form class=\"chernoff-form\">\n" +
+            "                                    <select class=\"form-control\" id=\"nose\"></select>\n" +
+            "                                </form>\n" +
+            "                            </div>\n" +
+            "                        </div>\n" +
+            "                    </li>\n" +
+            "\n" +
+            "                    <li class=\"list-group-item\">\n" +
+            "                        <span class=\"error\" id=\"error-eyebrow\"></span>\n" +
+            "                        <div class=\"row\">\n" +
+            "                            <div class=\"col-sm-1\" style=\"padding: 5px; padding-right: 40px\">\n" +
+            "                                <h4>brwi:</h4>\n" +
+            "                            </div>\n" +
+            "                            <div class=\"col-sm-9 panel-list-item\">\n" +
+            "                                <form class=\"chernoff-form\">\n" +
+            "                                    <select class=\"form-control\" id=\"eyebrow\"></select>\n" +
+            "                                </form>\n" +
+            "                            </div>\n" +
+            "                        </div>\n" +
+            "                    </li>\n" +
+            "\n" +
+            "                    <li class=\"list-group-item\">\n" +
+            "                        <span class=\"error\" id=\"error-head\"></span>\n" +
+            "                        <div class=\"row\">\n" +
+            "                            <div class=\"col-sm-1\" style=\"padding: 5px; padding-right: 40px\">\n" +
+            "                                <h4>głowa:</h4>\n" +
+            "                            </div>\n" +
+            "                            <div class=\"col-sm-9 panel-list-item\">\n" +
+            "                                <form class=\"chernoff-form\">\n" +
+            "                                    <select class=\"form-control\" id=\"head\"></select>\n" +
+            "                                </form>\n" +
+            "                            </div>\n" +
+            "                        </div>\n" +
+            "                    </li>\n" +
+            "\n" +
+            "                    <li class=\"list-group-item\">\n" +
+            "                        <button type=\"button\" id=\"generateFaces\" class=\"btn btn-primary btn-block\">\n" +
+            "                            Generuj twarze na mapie\n" +
+            "                        </button>\n" +
+            "                    </li>" +
+            "</ul>"
+        );
+
+        /**
+         * Generowanie opcji w selectach.
+         */
+        var options = getOptions(data, true);
+
+        var content = document.getElementsByClassName("chernoff-form").innerHTML;
+        content += "<option value=\"null\">\n";
+
+        for (var i = 0; i < options.length; i++)
+            content += "<option value=" + options[i] + ">" + options[i] + "\n";
+
+        $(".form-control").html(content);
+    }
 
     /**
      * Funkcja rysująca wszystkie twarze na mapie.
      * @param data - JSON z danymi.
      */
-    this.drawFaces = function (data) {
+    function drawFaces(data) {
 
-        var dataArrays = getDataArrays(data);
         var form = getFormValuesbyId("eyes", "mounth", "nose", "eyebrow", "head");
 
         if (form.validate) {
 
-            var parameters = getParameters(dataArrays, form.values);
+            var dataArrays = getDataArrays(data);
+            var parameters = getParameters(data, dataArrays, form.values);
 
             var map = new Image();
             map.src = "map.png";
@@ -54,7 +228,7 @@ function ChernoffFaces() {
             dynamicCanvas.height = "620";
             dynamicCanvas.width = "660";
 
-            var canvas = document.getElementById(canvasId);
+            var canvas = document.getElementById("mapCanvas");
             var ctx = canvas.getContext("2d");
 
             ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -77,38 +251,26 @@ function ChernoffFaces() {
                 // window.open(canvas.toDataURL());
             };
         }
-    };
+    }
 
     /**
-     * Funkcja tworząca tablice z danymi.
+     * Funkcja dynamicznie tworząca tablice z danymi.
      * @param data - JSON z danymi.
      * @return {{}} - Tablice z danymi.
      */
     function getDataArrays(data) {
 
         var dataArrays = {};
-        var wojewodztwo = [];
-        var powierzchnia = [];
-        var ludnosc = [];
-        var urbanizacja = [];
-        var bezrobocie = [];
-        var PKB = [];
+        var options = getOptions(data, false);
 
-        for (var item in data) {
-            wojewodztwo[item] = data[item].wojewodztwo;
-            powierzchnia[item] = data[item].powierzchnia;
-            ludnosc[item] = data[item].ludnosc;
-            urbanizacja[item] = data[item].urbanizacja;
-            bezrobocie[item] = data[item].bezrobocie;
-            PKB[item] = data[item].PKB;
+        for (var i = 0; i < options.length; i++)
+            dataArrays[options[i]] = [];
+
+        for (var i = 0; i < options.length; i++) {
+            for (var j = 0; j < data.length; j++) {
+                dataArrays[options[i]][j] = data[j][options[i]];
+            }
         }
-
-        dataArrays.wojewodztwo = wojewodztwo;
-        dataArrays.powierzchnia = powierzchnia;
-        dataArrays.ludnosc = ludnosc;
-        dataArrays.urbanizacja = urbanizacja;
-        dataArrays.bezrobocie = bezrobocie;
-        dataArrays.PKB = PKB;
 
         return dataArrays;
     }
@@ -392,103 +554,66 @@ function ChernoffFaces() {
      * @param form - Tablica zawierająca dane z formularza.
      * @return {{eyes: *, mounth: *, nose: *, eyebrow: *, head: *}} - JSON z informacją do jakiej części twarzy jakie dane zostały przypisane.
      */
-    function getParameters(dataArrays, form) {
+    function getParameters(data, dataArrays, form) {
         var eyes;
         var mounth;
         var nose;
         var eyebrow;
         var head;
+        var options = getOptions(data, false);
 
-        switch (form.eyesInput) {
-            case "powierzchnia":
-                eyes = dataArrays.powierzchnia;
-                break;
-            case "ludnosc":
-                eyes = dataArrays.ludnosc;
-                break;
-            case "urbanizacja":
-                eyes = dataArrays.urbanizacja;
-                break;
-            case "bezrobocie":
-                eyes = dataArrays.bezrobocie;
-                break;
-            case "PKB":
-                eyes = dataArrays.PKB;
-                break;
-        }
+        for (var i = 0; i < options.length; i++) {
+            if (form.eyesInput == options[i])
+                eyes = dataArrays[options[i]];
 
-        switch (form.mounthInput) {
-            case "powierzchnia":
-                mounth = dataArrays.powierzchnia;
-                break;
-            case "ludnosc":
-                mounth = dataArrays.ludnosc;
-                break;
-            case "urbanizacja":
-                mounth = dataArrays.urbanizacja;
-                break;
-            case "bezrobocie":
-                mounth = dataArrays.bezrobocie;
-                break;
-            case "PKB":
-                mounth = dataArrays.PKB;
-                break;
-        }
+            if (form.mounthInput == options[i])
+                mounth = dataArrays[options[i]];
 
-        switch (form.noseInput) {
-            case "powierzchnia":
-                nose = dataArrays.powierzchnia;
-                break;
-            case "ludnosc":
-                nose = dataArrays.ludnosc;
-                break;
-            case "urbanizacja":
-                nose = dataArrays.urbanizacja;
-                break;
-            case "bezrobocie":
-                nose = dataArrays.bezrobocie;
-                break;
-            case "PKB":
-                nose = dataArrays.PKB;
-                break;
-        }
+            if (form.noseInput == options[i])
+                nose = dataArrays[options[i]];
 
-        switch (form.eyebrowInput) {
-            case "powierzchnia":
-                eyebrow = dataArrays.powierzchnia;
-                break;
-            case "ludnosc":
-                eyebrow = dataArrays.ludnosc;
-                break;
-            case "urbanizacja":
-                eyebrow = dataArrays.urbanizacja;
-                break;
-            case "bezrobocie":
-                eyebrow = dataArrays.bezrobocie;
-                break;
-            case "PKB":
-                eyebrow = dataArrays.PKB;
-                break;
-        }
+            if (form.eyebrowInput == options[i])
+                eyebrow = dataArrays[options[i]];
 
-        switch (form.headInput) {
-            case "powierzchnia":
-                head = dataArrays.powierzchnia;
-                break;
-            case "ludnosc":
-                head = dataArrays.ludnosc;
-                break;
-            case "urbanizacja":
-                head = dataArrays.urbanizacja;
-                break;
-            case "bezrobocie":
-                head = dataArrays.bezrobocie;
-                break;
-            case "PKB":
-                head = dataArrays.PKB;
-                break;
+            if (form.headInput == options[i])
+                head = dataArrays[options[i]];
         }
 
         return {"eyes": eyes, "mounth": mounth, "nose": nose, "eyebrow": eyebrow, "head": head};
     }
+
+    /**
+     * Funkcja pobierająca klucze z obiektu .json, które będą potrzebne do wygenerowania opcji w selectach.
+     */
+    function getOptions(data, toDisplay) {
+
+        if (typeof Object.keys !== "function") {
+            (function () {
+                var hasOwn = Object.prototype.hasOwnProperty;
+                Object.keys = Object_keys;
+
+                function Object_keys(obj) {
+                    var keys = [], name;
+                    for (name in obj) {
+                        if (hasOwn.call(obj, name)) {
+                            keys.push(name.toString());
+                        }
+                    }
+                    return keys;
+                }
+            })();
+        }
+
+        var originalOptions = Object.keys(data[0]);
+        originalOptions.splice(0, 2);
+
+        var optionsForArrays = [];
+        for (var i = 0; i < originalOptions.length; i++) {
+            optionsForArrays.push(originalOptions[i].toString().replace(/\s/g, ''));
+        }
+
+        if (toDisplay) return originalOptions;
+        return optionsForArrays;
+    }
+
 }
